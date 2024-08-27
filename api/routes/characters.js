c859d385615c52adc74158b12436ada075ab9bc6
Arthur Duarte <tuiduarte@gmail.com>
2024-08-27 10:45:23 -0400

    feat(routes/characters): add search by character name

--- api/routes/characters.js
+++ api/routes/characters.js
@@ -2,9 +2,21 @@ var express = require("express");
 var router = express.Router();
 const data = require('../data/characters.json')
 
-// Get characters
+// Get all characters
 router.get('/', function (req, res, next) {
     res.json(data);
 });
+
+// Get character by name
+router.get('/:name', (req, res) => {
+    const requestedCharacter = req.params.name
+    const response = data.find(character => character.name === requestedCharacter)
+
+    if (response) {
+        res.send(response);
+    } else {
+        res.status(404).send({ error: "Character not found" });
+    }
+})
 
 module.exports = router;
