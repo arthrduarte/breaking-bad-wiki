2cc9618909656d9182e6fdcd3ba745bb07308fdd
Arthur Duarte <tuiduarte@gmail.com>
2024-08-27 20:42:56 -0400

    style: add style to each card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
+import CharacterDetails from './components/CharacterDetails';
+import CharacterCard from './components/CharacterCard';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import axios from 'axios';
-import CharacterCard from './components/CharacterCard';
-import CharacterDetails from './components/CharacterDetails';
 
 interface Character {
   name: string;
@@ -39,7 +39,7 @@ function App() {
         <Route path="/" element={
           <>
             <h1 className="font-bold text-[3rem] text-center">Breaking Bad Wiki</h1>
-            <div className='flex flex-col justify-center items-center lg:grid lg:grid-cols-4 lg:gap-5'>
+            <div className='flex flex-col justify-center items-center lg:flex-row lg:flex-wrap'>
               {data.length > 0 ? (
                 data.map((character, index) => (
                   <CharacterCard key={index} index={index} name={character.name} image_url={character.image_url} portrayed={character.portrayed} detailsUrl={`/characters/${index}/${encodeURIComponent(character.name.toLowerCase().replace(/\s+/g, '-'))}`} />
--- client/src/components/CharacterCard.tsx
+++ client/src/components/CharacterCard.tsx
@@ -11,11 +11,13 @@ interface CharacterCardProps {
 
 export default function CharacterCard({ index, name, image_url, portrayed, detailsUrl }: CharacterCardProps) {
     return (
-        <Link to={detailsUrl}>
-            <div className='flex flex-col justify-center items-center shadow-xl'>
-                <h2>{name}</h2>
-                <img src={image_url} alt={name} />
-                <p>Portrayed by: {portrayed}</p>
+        <Link to={detailsUrl} className='flex justify-center mx-3 my-3 shadow-2xl'>
+            <div className='relative w-[200px] h-[260px] '>
+                <div className='relative w-full h-full'>
+                    <img src={image_url} alt={name} className='w-full h-full object-cover rounded-t-xl' />
+                    <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-50'></div>
+                </div>
+                <div className="absolute bottom-0 left-0 p-4 text-white">{name}</div>
             </div>
         </Link>
     )
